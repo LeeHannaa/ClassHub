0931fa011e10c4a5428b155c772cc32eb1441652
uojin29 <uojin29@gmail.com>
2024-03-25 15:40:49 +0900

    feat: post를 생성할 때 생성된 태그 정보 저장 및 @Transactional 추가 (#28)

--- src/main/java/com/example/classhub/domain/post/service/PostService.java
+++ src/main/java/com/example/classhub/domain/post/service/PostService.java
@@ -14,6 +14,7 @@ import com.example.classhub.domain.post.repository.PostRepository;
 import com.example.classhub.domain.tag.ClassHub_Tag;
 import com.example.classhub.domain.tag.dto.TagDto;
 import com.example.classhub.domain.tag.service.TagService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -48,7 +49,8 @@ public class PostService {
         return filenameParts.length > 1 ? filenameParts[filenameParts.length - 1] : ""; // 확장자
     }
 
-    public void processCsvFile(ClassHub_LRoom lRoom, MultipartFile csvFile) {
+    public String processCsvFile(ClassHub_LRoom lRoom, MultipartFile csvFile) {
+        StringBuilder tagIdsBuilder = new StringBuilder();
         try (BOMInputStream bomInputStream = new BOMInputStream(csvFile.getInputStream());
              BufferedReader fileReader = new BufferedReader(new InputStreamReader(bomInputStream, StandardCharsets.UTF_8));
              CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
@@ -59,48 +61,61 @@ public class PostService {
                         .name(headerName)
                         .lectureRoom(lRoom)
                         .build();
-                tagService.createTag(TagDto.from(tag), lRoom.getLRoomId());
+                TagDto createdTagDto = tagService.createTag(TagDto.from(tag), lRoom.getLRoomId()); // 생성된 태그 정보 가져오기
+                Long tagId = createdTagDto.getTagId(); // 생성된 태그의 아이디 가져오기
+                tagIdsBuilder.append(tagId).append(",");
             }
         } catch (IOException e) {
             throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
         }
+        String tagIds = tagIdsBuilder.toString();
+        if (tagIds.endsWith(",")) {
+            tagIds = tagIds.substring(0, tagIds.length() - 1); // 마지막 쉼표 제거
+        }
+        return tagIds;
     }
 
+    @Transactional
     public void savePost(PostDto postDto, MultipartFile file) {
-        ClassHub_LRoom lRoom = lectureRoomRepository.findById(postDto.getLRoomId()).orElseThrow(() -> new IllegalArgumentException("해당 강의실이 존재하지 않습니다."));
-        ClassHub_Post post = ClassHub_Post.from(postDto, lRoom);
-        postRepository.save(post);
+        ClassHub_LRoom lRoom = lectureRoomRepository.findById(postDto.getLRoomId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 강의실이 존재하지 않습니다."));
 
         if (!file.isEmpty()) {
+            String tagIds = processCsvFile(lRoom, file);
+            ClassHub_Post post = ClassHub_Post.from(postDto, lRoom, tagIds);
+            postRepository.save(post);
+
             FileDataDto fileDataDto = FileDataDto.builder()
                     .fileDataName(getFileDataName(file))
                     .fileDataType(getFileDataType(file))
                     .postId(post.getPostId())
                     .build();
             fileDataService.saveFileData(fileDataDto);
-            processCsvFile(lRoom, file);
+        } else {
+            ClassHub_Post post = ClassHub_Post.from(postDto, lRoom);
+            postRepository.save(post);
         }
     }
-
-   public PostListResponse getPostList(){
+    @Transactional
+    public PostListResponse getPostList(){
         List<ClassHub_Post> posts = postRepository.findAll();
         List<PostResponse> postResponses = posts.stream()
                 .map(PostResponse::new)
                 .toList();
         return new PostListResponse(postResponses);
    }
-
+    @Transactional
     public PostDto findByPostId(Long postId) {
         ClassHub_Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
         return PostDto.from(post);
     }
-
+    @Transactional
     public void update(Long postId, PostDto from) {
         ClassHub_Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
         post.update(from);
         postRepository.save(post);
     }
-
+    @Transactional
     public void delete(Long postId) {
         postRepository.deleteById(postId);
     }
