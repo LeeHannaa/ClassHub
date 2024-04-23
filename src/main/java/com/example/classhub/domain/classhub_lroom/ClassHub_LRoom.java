4b50ab2988afefc46dc6d4721bc6cd7c081ab48e
uojin29 <uojin29@gmail.com>
2024-04-23 14:59:00 +0900

    feat: 강의실 별로 학생 기본 정보 저장, csv 양식 다운로드

--- src/main/java/com/example/classhub/domain/classhub_lroom/ClassHub_LRoom.java
+++ src/main/java/com/example/classhub/domain/classhub_lroom/ClassHub_LRoom.java
@@ -28,6 +28,7 @@ public class ClassHub_LRoom extends BaseEntity {
     private String creator;
     private String description;
     private boolean onOff;
+    private String studentInfo;
 
     @OneToMany(mappedBy = "lectureRoom")
     private List<ClassHub_Tag> tags = new ArrayList<>();
@@ -42,6 +43,7 @@ public class ClassHub_LRoom extends BaseEntity {
                 .stInviteCode(stInviteCode)
                 .description(lectureRoomDto.getDescription())
                 .onOff(lectureRoomDto.isOnOff())
+                .studentInfo(lectureRoomDto.getStudentInfo())
                 .build();
     }
 
@@ -52,6 +54,7 @@ public class ClassHub_LRoom extends BaseEntity {
                 .stInviteCode(lectureRoomDto.getStInviteCode())
                 .description(lectureRoomDto.getDescription())
                 .onOff(lectureRoomDto.isOnOff())
+                .studentInfo(lectureRoomDto.getStudentInfo())
                 .build();
     }
 
@@ -61,5 +64,6 @@ public class ClassHub_LRoom extends BaseEntity {
         this.onOff = lectureRoomDto.isOnOff();
         this.stInviteCode = lectureRoomDto.getStInviteCode();
         this.taInviteCode = lectureRoomDto.getTaInviteCode();
+        this.studentInfo = lectureRoomDto.getStudentInfo();
     }
 }
