bc285e45c5be3ec5c290b0ecfb7a39c615d36fed
00KANGJK <kth00021000@gmail.com>
2024-05-07 06:34:45 +0900

    edit: 테이블 한국어로 변경

--- src/main/resources/static/bootstrap/js/demo/datatables-demo.js
+++ src/main/resources/static/bootstrap/js/demo/datatables-demo.js
@@ -1,5 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable({
+    // 한 페이지에 보여지는 목록 수 조절 기능 비활성화
+    "language": {
+      "paginate": {
+        "next": "다음",
+        "previous": "이전",
+        "first": "첫 페이지",
+        "last": "마지막 페이지"
+      },
+      "search": "검색"
+    },
+    "lengthChange": false,
+    "info": false
   });
 });
