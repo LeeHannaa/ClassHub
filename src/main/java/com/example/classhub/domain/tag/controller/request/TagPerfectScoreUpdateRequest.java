05b5b15d5c24295e2adfc931394337befdc742c0
00KANGJK <kth00021000@gmail.com>
2024-05-07 12:43:09 +0900

    feat: 만점 점수 수정 기능 추가, 데이터 테이블 한국어로 통일

---
+++ src/main/java/com/example/classhub/domain/tag/controller/request/TagPerfectScoreUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.example.classhub.domain.tag.controller.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class TagPerfectScoreUpdateRequest {
+    private int perfectScore;
+}
