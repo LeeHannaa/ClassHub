3e8067c51519896c9c6d4d624a6e542c73a838e3
LeeHannaa <103620720+LeeHannaa@users.noreply.github.com>
2024-05-20 20:20:33 +0900

    feat: 학생 관리 페이징 처리

--- src/main/java/com/example/classhub/domain/memberlroom/repository/MemberLRoomRepository.java
+++ src/main/java/com/example/classhub/domain/memberlroom/repository/MemberLRoomRepository.java
@@ -1,6 +1,8 @@
 package com.example.classhub.domain.memberlroom.repository;
 
 import com.example.classhub.domain.memberlroom.ClassHub_MemberLRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.Optional;
 
 public interface MemberLRoomRepository extends JpaRepository<ClassHub_MemberLRoom, Long> {
   Optional<ClassHub_MemberLRoom> findByClassHubMember_MemberIdAndLectureRoom_lRoomId(Long classHubMemberId, Long lRoomId); // 반환 유형 수정
-  List<ClassHub_MemberLRoom> findByLectureRoom_lRoomId(Long lRoomId);
+  Page<ClassHub_MemberLRoom> findByLectureRoom_lRoomId(Long lRoomId, Pageable pageable);
   ClassHub_MemberLRoom findByLectureRoom_lRoomIdAndClassHubMember_UniqueId(Long lRoomId, String uniqueId);
 
 }
