92757f71d447cbdaf4119d05cd651d9a4a7499a5
00KANGJK <kth00021000@gmail.com>
2024-06-04 14:38:37 +0900

    feat: 자신의 데이터가 없는 tag 정도는 안불러오는 것으로 수정

--- src/main/java/com/example/classhub/domain/tag/repository/TagRepository.java
+++ src/main/java/com/example/classhub/domain/tag/repository/TagRepository.java
@@ -18,6 +18,9 @@ public interface TagRepository extends JpaRepository<ClassHub_Tag, Long> {
 
     @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM ClassHub_Tag t WHERE t.name = :name AND t.lectureRoom.lRoomId = :lRoomId")
     boolean existsByNameAndLectureRoom_LRoomId(@Param("name") String name, @Param("lRoomId") Long lRoomId);
-  
+
     List<ClassHub_Tag> findByTagIdIn(List<Long> tagIdList);
+
+    @Query("SELECT t FROM ClassHub_Tag t JOIN t.dataDetails d WHERE d.studentNum = :studentNum")
+    List<ClassHub_Tag> findByStudentNum(@Param("studentNum") String studentNum);
 }
